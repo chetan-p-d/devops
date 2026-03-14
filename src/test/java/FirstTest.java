0a4931135d3c584d6924fecd431d7d68f280d7fd
Chetan <chetupd9632@gmail.com>
2026-03-14 12:32:45 +0530

    initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void m1() {
+        System.out.println("m1 Executed");
+    }
+    @Test
+    public void m2() {
+        System.out.println("m2 Executed");
+    }
+}
---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class SecondTest {
+    @Test
+    public void m3() {
+        System.out.println("m3 Executed");
+    }
+    @Test
+    public void m4() {
+        System.out.println("m4 Executed");
+    }
+}
